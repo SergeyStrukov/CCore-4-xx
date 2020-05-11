20caa6e9eaa1aa24317fc8791781173454b2018a
Sergey Strukov <sshimnick@hotmail.com>
2020-05-11 09:16:12 +0300

    dummy

---
+++ Fundamental/CCore/inc/Exception.h
@@ -0,0 +1,34 @@
+/* Exception.h */
+//----------------------------------------------------------------------------------------
+//
+//  Project: CCore 4.01
+//
+//  Tag: Simple Mini
+//
+//  License: Boost Software License - Version 1.0 - August 17th, 2003
+//
+//            see http://www.boost.org/LICENSE_1_0.txt or the local copy
+//
+//  Copyright (c) 2020 Sergey Strukov. All rights reserved.
+//
+//----------------------------------------------------------------------------------------
+
+#ifndef CCore_inc_Exception_h
+#define CCore_inc_Exception_h
+
+#include <CCore/inc/gadget/ExceptionType.h>
+
+// dummy header
+
+namespace CCore {
+
+/* Printf() */
+
+template <class ... TT>
+void Printf(const TT & ...) {}
+
+} // namespace CCore
+
+#endif
+
+
