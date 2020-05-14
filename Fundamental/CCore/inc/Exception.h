6d3f78ba9b26abf564754b75bf004ee38941bc3d
Sergey Strukov <sshimnick@hotmail.com>
2020-05-14 10:59:12 +0300

    initial

--- Fundamental/CCore/inc/Exception.h
+++ Fundamental/CCore/inc/Exception.h
@@ -3,7 +3,7 @@
 //
 //  Project: CCore 4.01
 //
-//  Tag: Simple Mini
+//  Tag: Fundamental Mini
 //
 //  License: Boost Software License - Version 1.0 - August 17th, 2003
 //
@@ -16,16 +16,190 @@
 #ifndef CCore_inc_Exception_h
 #define CCore_inc_Exception_h
 
-#include <CCore/inc/gadget/ExceptionType.h>
-
-// dummy header
+#include <CCore/inc/Printf.h>
+#include <CCore/inc/PlanInit.h>
 
 namespace CCore {
 
-/* Printf() */
+/* GetPlanInitNode_...() */
 
-template <class ... TT>
-void Printf(const TT & ...) {}
+PlanInitNode * GetPlanInitNode_Exception();
+
+/* classes */
+
+class ReportException;
+
+class SilentReportException;
+
+template <class P> class ReportExceptionTo;
+
+class PrintException;
+
+template <class T> class CustomThrow;
+
+template <class T> class PrintCustomException;
+
+/* class ReportException */
+
+class ReportException : NoCopy
+ {
+   DLink<ReportException> link;
+
+   bool nok = false ;
+
+   using Algo = DLink<ReportException>::LinearAlgo<&ReportException::link> ;
+
+  private:
+
+   static ReportException * Top();
+
+   static void SetTop(ReportException *top);
+
+   static ReportException * Start();
+
+   static void Add(ReportException *report,const char *ptr,ulen len);
+
+   static void End(ReportException *report);
+
+   friend class PrintException;
+
+  protected:
+
+   static void Print(StrLen str);
+
+   virtual void print(StrLen str);
+
+   virtual void start(ExceptionType ex);
+
+   virtual void add(const char *ptr,ulen len);
+
+   virtual void end();
+
+  public:
+
+   ReportException() noexcept;
+
+   ~ReportException();
+
+   bool operator + () const { return !nok; }
+
+   bool operator ! () const { return nok; }
+
+   void clear() { nok=false; }
+
+   void guard() { if( nok ) throw CatchType(); }
+
+   static void Clear();
+ };
+
+/* class SilentReportException */
+
+class SilentReportException : public ReportException
+ {
+  private:
+
+   virtual void print(StrLen str);
+
+   virtual void start(ExceptionType ex);
+
+   virtual void add(const char *ptr,ulen len);
+
+   virtual void end();
+
+  public:
+
+   SilentReportException();
+
+   ~SilentReportException();
+ };
+
+/* class ReportExceptionTo<P> */
+
+template <class P>
+class ReportExceptionTo : public ReportException
+ {
+   P &out;
+
+  public:
+
+   explicit ReportExceptionTo(P &out_) : out(out_) {}
+
+   ~ReportExceptionTo() {}
+
+   virtual void print(StrLen str)
+    {
+     SilentReportException report;
+
+     try
+       {
+        out.put(str.ptr,str.len);
+       }
+     catch(...) {}
+    }
+ };
+
+/* class PrintException */
+
+class PrintException : public PrintBase
+ {
+   ExceptionType ex;
+   ReportException *report;
+
+   char buf[TextBufLen];
+   bool do_throw;
+
+  private:
+
+   virtual PtrLen<char> do_provide(ulen hint_len);
+
+   virtual void do_flush(char *ptr,ulen len);
+
+   void start();
+
+  protected:
+
+   PrintException();
+
+  public:
+
+   explicit PrintException(ExceptionType ex);
+
+   ~PrintException() noexcept(false);
+ };
+
+template <>
+struct PrintOutAdapter<ExceptionType>
+ {
+  using PrintOutType = PrintException ;
+ };
+
+/* class CustomThrow<T> */
+
+template <class T>
+class CustomThrow : NoCopy
+ {
+   T obj;
+
+  public:
+
+   template <class ... SS>
+   explicit CustomThrow(SS && ... ss) : obj( std::forward<SS>(ss)... ) {}
+
+   ~CustomThrow() noexcept(false) { throw obj; }
+ };
+
+/* class PrintCustomException<T> */
+
+template <class T>
+class PrintCustomException : public CustomThrow<T> , public PrintException
+ {
+  public:
+
+   template <class ... SS>
+   explicit PrintCustomException(SS && ... ss) : CustomThrow<T>( std::forward<SS>(ss)... ) {}
+
+   ~PrintCustomException() noexcept(false) {}
+ };
 
 } // namespace CCore
 
