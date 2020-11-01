b2cb151b6a6c8e6bd2fb539f3bb0c5ff500363af
Sergey Strukov <sshimnick@hotmail.com>
2020-11-01 13:48:41 +0300

    initial

---
+++ Target/WIN32/CCore/test/test6003.Char.cpp
@@ -0,0 +1,56 @@
+/* test6003.Char.cpp */
+//----------------------------------------------------------------------------------------
+//
+//  Project: CCore 4.01
+//
+//  Tag: Target/WIN32
+//
+//  License: Boost Software License - Version 1.0 - August 17th, 2003
+//
+//            see http://www.boost.org/LICENSE_1_0.txt or the local copy
+//
+//  Copyright (c) 2020 Sergey Strukov. All rights reserved.
+//
+//----------------------------------------------------------------------------------------
+
+#include <CCore/test/test.h>
+
+#include <CCore/inc/Utf8.h>
+#include <CCore/inc/Sort.h>
+#include <CCore/inc/Array.h>
+
+namespace App {
+
+namespace Private_6003 {
+
+
+} // namespace Private_6003
+
+using namespace Private_6003;
+
+/* Testit<6003> */
+
+template<>
+const char *const Testit<6003>::Name="Test6003 Char";
+
+template<>
+bool Testit<6003>::Main()
+ {
+  StrLen text="абвгдеёжзийклмнопрстуфхцчшщъыьэюя"
+              "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ"_c;
+
+  DynArray<Unicode> buf;
+
+  while( +text ) buf.append_copy(CutUtf8_unicode(text));
+
+  Sort(Range(buf));
+
+  for(Unicode sym : buf ) Putobj(Con,ToUtf8(sym));
+
+  Putch(Con,'\n');
+
+  return true;
+ }
+
+} // namespace App
+
---
+++ Target/WIN32/CCore/test/test6006.IntelRandom.cpp
@@ -0,0 +1,54 @@
+/* test6006.IntelRandom.cpp */
+//----------------------------------------------------------------------------------------
+//
+//  Project: CCore 4.01
+//
+//  Tag: Target/WIN32
+//
+//  License: Boost Software License - Version 1.0 - August 17th, 2003
+//
+//            see http://www.boost.org/LICENSE_1_0.txt or the local copy
+//
+//  Copyright (c) 2020 Sergey Strukov. All rights reserved.
+//
+//----------------------------------------------------------------------------------------
+
+#include <CCore/test/test.h>
+
+#include <CCore/inc/crypton/IntelCPUID.h>
+#include <CCore/inc/crypton/IntelRandom.h>
+
+namespace App {
+
+/* Testit<6006> */
+
+template<>
+const char *const Testit<6006>::Name="Test6006 IntelRandom";
+
+template<>
+bool Testit<6006>::Main()
+ {
+  Crypton::IntelCPUID1 cpuid;
+
+  Printf(Con,"EAX=#.h; EBX=#.h; ECX=#.h; EDX=#.h;\n",cpuid.EAX,cpuid.EBX,cpuid.ECX,cpuid.EDX);
+
+  Printf(Con,"TIMESTAMP = #;\n",(bool)cpuid.capableTIMESTAMP());
+  Printf(Con,"MMX = #;\n",(bool)cpuid.capableMMX());
+  Printf(Con,"SSE = #;\n",(bool)cpuid.capableSSE());
+  Printf(Con,"SSE2 = #;\n",(bool)cpuid.capableSSE2());
+  Printf(Con,"SSE3 = #;\n",(bool)cpuid.capableSSE3());
+  Printf(Con,"PCLMULQDQ = #;\n",(bool)cpuid.capablePCLMULQDQ());
+  Printf(Con,"SSSE3 = #;\n",(bool)cpuid.capableSSSE3());
+  Printf(Con,"SSE4_1 = #;\n",(bool)cpuid.capableSSE4_1());
+  Printf(Con,"SSE4_2 = #;\n",(bool)cpuid.capableSSE4_2());
+  Printf(Con,"AVX = #;\n",(bool)cpuid.capableAVX());
+
+  Crypton::IntelRandom random;
+
+  for(unsigned cnt=10; cnt ;cnt--) Printf(Con,"#.h;\n",random.next());
+
+  return true;
+ }
+
+} // namespace App
+
