c6c35551770ad1394aa233c2e93525ddfc069ddb
Sergey Strukov <sshimnick@hotmail.com>
2022-06-05 23:47:35 +0300

    initial

---
+++ Applied/CCore/test/test3042.Radix2FFT.cpp
@@ -0,0 +1,39 @@
+/* test3042.Radix2FFT.cpp */
+//----------------------------------------------------------------------------------------
+//
+//  Project: CCore 4.01
+//
+//  Tag: Applied
+//
+//  License: Boost Software License - Version 1.0 - August 17th, 2003
+//
+//            see http://www.boost.org/LICENSE_1_0.txt or the local copy
+//
+//  Copyright (c) 2022 Sergey Strukov. All rights reserved.
+//
+//----------------------------------------------------------------------------------------
+
+#include <CCore/test/test.h>
+
+namespace App {
+
+namespace Private_3042 {
+
+
+} // namespace Private_3042
+
+using namespace Private_3042;
+
+/* Testit<3042> */
+
+template<>
+const char *const Testit<3042>::Name="Test3042 Radix2FFT";
+
+template<>
+bool Testit<3042>::Main()
+ {
+  return true;
+ }
+
+} // namespace App
+
